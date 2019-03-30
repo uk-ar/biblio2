2a9bdf1532a3d0ba134765c57b28b0dae50fc37b
uk-ar <yuuki.ari@gmail.com>
2019-03-30 09:32:57 +0900

    Revert "Connect firebase"
    This reverts commit 891ea0637bd84079bb5827e348e4a33f1b0ef03e.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+final dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
 
 class MyApp extends StatelessWidget {
   @override
@@ -31,25 +39,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildBody(BuildContext context) {
     // TODO: get actual snapshot from Cloud Firestore
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('baby').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
+    return _buildList(context, dummySnapshot);
   }
 
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  Widget _buildList(BuildContext context, List<Map> snapshot) {
     return ListView(
       padding: const EdgeInsets.only(top: 20.0),
       children: snapshot.map((data) => _buildListItem(context, data)).toList(),
     );
   }
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
+  Widget _buildListItem(BuildContext context, Map data) {
+    final record = Record.fromMap(data);
+
     return Padding(
         key: ValueKey(record.name),
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
@@ -59,17 +61,18 @@ class _MyHomePageState extends State<MyHomePage> {
             borderRadius: BorderRadius.circular(5.0),
           ),
           child: ListTile(
-              title: Text(record.name),
-              trailing: Text(record.votes.toString()),
-              onTap: () =>
-                  Firestore.instance.runTransaction((transaction) async {
-                    final freshSnapshot =
-                        await transaction.get(record.reference);
-                    final fresh = Record.fromSnapshot(freshSnapshot);
+            title: Text(record.name),
+            trailing: Text(record.votes.toString()),
+            // onTap: () =>
+            //     Firestore.instance.runTransaction((transaction) async {
+            //       final freshSnapshot =
+            //           await transaction.get(record.reference);
+            //       final fresh = Record.fromSnapshot(freshSnapshot);
 
-                    await transaction
-                        .update(record.reference, {'votes': fresh.votes + 1});
-                  })),
+            //       await transaction
+            //           .update(record.reference, {'votes': fresh.votes + 1});
+            //     })
+          ),
         ));
   }
 }
@@ -77,16 +80,16 @@ class _MyHomePageState extends State<MyHomePage> {
 class Record {
   final String name;
   final int votes;
-  final DocumentReference reference;
+  //final DocumentReference reference;
 
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
+  Record.fromMap(Map<String, dynamic> map) // ,{this.reference})
       : assert(map['name'] != null),
         assert(map['votes'] != null),
         name = map['name'],
         votes = map['votes'];
 
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
+  //Record.fromSnapshot(DocumentSnapshot snapshot)
+  //    : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   @override
   String toString() => "Record<$name:$votes>";
