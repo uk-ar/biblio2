5b35232bff8769c9a9a6be03084ea6963731cf62
uk-ar <yuuki.ari@gmail.com>
2019-04-11 05:24:47 +0900

    Enable Post

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -12,7 +12,45 @@ import Firebase
 import MobileCoreServices
 
 class ShareViewController: SLComposeServiceViewController {
-
+    /** @var handle
+     @brief The handler for the auth state listener, to allow cancelling later.
+     */
+    //https://github.com/firebase/snippets-ios/blob/7af7f641151501af427fc4ef7c421b0ba357ce27/firestore/swift/firestore-smoketest/ViewController.swift#L140-L152
+    var db: Firestore!
+    var handle: AuthStateDidChangeListenerHandle?
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+            db = Firestore.firestore()
+        }
+        // [START auth_listener]
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            // [START_EXCLUDE]
+            if (user == nil) {
+                print("fireauth:no current")
+                Auth.auth().signInAnonymously() { (authResult, error) in
+                    if let user = authResult?.user {
+                        let isAnonymous = user.isAnonymous  // true
+                        let uid = user.uid
+                        self.db.collection("users").document(uid).setData([
+                            "isAnonymous": isAnonymous,
+                            ])
+                    }
+                }
+            }else{
+                 print("fireauth:current")
+            }
+            // [END_EXCLUDE]
+        }
+        // [END auth_listener]
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // [START remove_auth_listener]
+        Auth.auth().removeStateDidChangeListener(handle!)
+        // [END remove_auth_listener]
+    }
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
         return true
@@ -21,36 +59,21 @@ class ShareViewController: SLComposeServiceViewController {
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
         // Use Firebase library to configure APIs
-        if FirebaseApp.app() == nil {
-            FirebaseApp.configure()
-        }
+
+
         let extensionItem: NSExtensionItem = self.extensionContext?.inputItems.first as! NSExtensionItem
         let itemProvider = extensionItem.attachments?.first as! NSItemProvider
         
         let puclicURL = String(kUTTypeURL)  // "public.url"
 //        var ref: DocumentReference? = nil
 //
-        print("fireauth:which")
-        var user = Auth.auth().currentUser
-        if (user == nil){
-            print("fireauth:no current")
-            Auth.auth().signInAnonymously() { (authResult, error) in
-                user = authResult?.user
-                let isAnonymous = user?.isAnonymous  // true
-                let uid = user?.uid
-                print("fireauth:user:",user)
-                print("fireauth:uid:",uid)
-                self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-            }
-        }else{
-            print("fireauth:current")
-            print("fireauth:user:",user)
-            print("fireauth:uid:",user?.uid)
-            self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        let user = Auth.auth().currentUser
+        guard let uid = user?.uid else{
+            return
         }
-        
-        
-        let db = Firestore.firestore()
+        print("fireauth:user:",user)
+        print("fireauth:uid:",uid)
+        let userRef: DocumentReference? = db.collection("users").document(uid)
         // shareExtension で NSURL を取得
         if itemProvider.hasItemConformingToTypeIdentifier(puclicURL) {
             itemProvider.loadItem(forTypeIdentifier: puclicURL, options: nil, completionHandler: { (item, error) in
@@ -59,15 +82,15 @@ class ShareViewController: SLComposeServiceViewController {
                     // ----------
                     // 保存処理
                     // ----------
-                    db.collection("books").document("isbnXX").setData([
-                        "title": "GitHub",
+                    self.db.collection("posts").addDocument(data: [
+                        "author": userRef,
                         "url": url.absoluteString ?? ""
-                    ]) { err in
+                        ]){ err in
                         if let err = err {
                             print("fireauth:Error writing document: \(err)")
                         } else {
                             print("fireauth:Document successfully written!")
-                            //self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+                            self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
                         }
                     }
                 }
