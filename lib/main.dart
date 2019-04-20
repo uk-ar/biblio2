5d095213e9454d1f38e99d6eafa180287f43c36f
uk-ar <yuuki.ari@gmail.com>
2019-04-21 05:04:45 +0900

    Add bloc

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,35 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
+import 'package:bloc_provider/bloc_provider.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
+}
+
+class CounterBloc implements Bloc {
+  //final _countController = BehaviorSubject<int>(seedValue: 0);
+  final _countController = BehaviorSubject<int>.seeded(0);
+  final _incrementController = PublishSubject<void>();
+
+  CounterBloc() {
+    _incrementController
+        .scan<int>((sum, _v, _i) => sum + 1, 0)
+        .pipe(_countController);
+  }
+
+  ValueObservable<int> get count => _countController;
+  Sink<void> get increment => _incrementController.sink;
+
+  @override
+  void dispose() async {
+    await _incrementController.close();
+    await _countController.close();
+  }
 }
 
 class _MyAppState extends State<MyApp> {
